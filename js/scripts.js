f57a7fd9019b9667bca03078603c22d946996f00
VadimIshkarin <theishkarin@gmail.com>
2022-06-09 21:49:44 -0400

    I made changes to script.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert("Hello world");
-
-let favoriteFood = "Avocado";
-document.write(favoriteFood);
+let pokemonList = [
+  { name: "Bulbasaur", height: 7, color: "blue", type: ["grass", "poison"] },
+  { name: "Charmander", height: 6, color: "orange", type: "fire" },
+  { name: "Charizard", height: 8, color: "brown", type: ["fire", "flying"] },
+];
