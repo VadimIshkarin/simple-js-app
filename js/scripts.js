5a4e6b337a45eb700d22258fd374390ffab951e9
VadimIshkarin <theishkarin@gmail.com>
2022-06-13 18:58:32 -0400

    I changed the code fourth time in scripts.js file, used document write () instead of console.log

--- js/scripts.js
+++ js/scripts.js
@@ -9,22 +9,51 @@ let pokemonList = [
 
 for (let i = 0; i < pokemonList.length; i++) {
   if (pokemonList[i].height < 8 && pokemonList[i].height > 6) {
-    document.write("<p>" + pokemonList[0].name + ", " + "<p/>");
-    console.log(
+    document.write(
       `${pokemonList[i].name} height is ${pokemonList[i].height}, color is ${pokemonList[i].color}, type is ${pokemonList[i].type}.`
     );
   } else if (pokemonList[i].height < 7 && pokemonList[i].height > 5) {
-    document.write("<p>" + pokemonList[1].name + ", " + "<p/>");
-    console.log(
-      `${pokemonList[i].name} height is ${pokemonList[i].height}, color is ${pokemonList[i].color}, type is ${pokemonList[i].type}.`
+    document.write(
+      `${"<p>" + pokemonList[i].name} height is ${
+        pokemonList[i].height
+      }, color is ${pokemonList[i].color}, type is ${pokemonList[i].type}.`
     );
   } else {
-    document.write("<p>" + pokemonList[2].name + ". " + "<p/>");
-    console.log(
-      `${pokemonList[i].name} height is ${pokemonList[i].height}: - Wow, that's big!, color is ${pokemonList[i].color}, type is ${pokemonList[i].type}.`
+    document.write(
+      `${"<p>" + pokemonList[i].name} height is ${
+        pokemonList[i].height
+      }: - Wow, that's big!, color is ${pokemonList[i].color}, type is ${
+        pokemonList[i].type
+      }.`
     );
   }
 }
+
+// //FOR LOOP that iterates over each item in pokemonList (an array of objects)
+// let pokemonList = [
+//   { name: "Bulbasaur", height: 7, color: "blue", type: "poison" },
+//   { name: "Charmander", height: 6, color: "orange", type: "fire" },
+//   { name: "Charizard", height: 8, color: "brown", type: ["flying", " fire"] },
+// ];
+
+// for (let i = 0; i < pokemonList.length; i++) {
+//   if (pokemonList[i].height < 8 && pokemonList[i].height > 6) {
+//     document.write("<p>" + pokemonList[0].name + ", " + "<p/>");
+//     console.log(
+//       `${pokemonList[i].name} height is ${pokemonList[i].height}, color is ${pokemonList[i].color}, type is ${pokemonList[i].type}.`
+//     );
+//   } else if (pokemonList[i].height < 7 && pokemonList[i].height > 5) {
+//     document.write("<p>" + pokemonList[1].name + ", " + "<p/>");
+//     console.log(
+//       `${pokemonList[i].name} height is ${pokemonList[i].height}, color is ${pokemonList[i].color}, type is ${pokemonList[i].type}.`
+//     );
+//   } else {
+//     document.write("<p>" + pokemonList[2].name + ". " + "<p/>");
+//     console.log(
+//       `${pokemonList[i].name} height is ${pokemonList[i].height}: - Wow, that's big!, color is ${pokemonList[i].color}, type is ${pokemonList[i].type}.`
+//     );
+//   }
+// }
 
 //Better to use FOR OF LOOP:
 //FOR OF LOOP that iterates over each item in pokemonList (an array of objects)
