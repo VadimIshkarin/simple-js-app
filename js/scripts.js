7d9d9b48053401e047a4c05a86a8b5225c486ec6
VadimIshkarin <theishkarin@gmail.com>
2022-06-15 12:00:28 -0400

    Part 1: Use forEach() Loop instead of for loop in script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,29 @@
-//Never use FOR LOOP with Arrays, better to use Array helper methods such as forEach, map, reduce!!!
-
-//FOR LOOP that iterates over each item in pokemonList (an array of objects) and document write () pokemon name, color, height, type and don't use console.log.
+//FOR EACH method that iterates over each item in pokemonList (an array of objects).
 let pokemonList = [
   { name: "Bulbasaur", height: 7, color: "blue", type: "poison" },
   { name: "Charmander", height: 6, color: "orange", type: "fire" },
   { name: "Charizard", height: 8, color: "brown", type: ["flying", " fire"] },
 ];
 
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height < 8 && pokemonList[i].height > 6) {
+pokemonList.forEach(function (pokemon) {
+  if (pokemon.height >= 8) {
     document.write(
-      `${pokemonList[i].name} height is ${pokemonList[i].height}, color is ${pokemonList[i].color}, type is ${pokemonList[i].type}.`
-    );
-  } else if (pokemonList[i].height < 7 && pokemonList[i].height > 5) {
-    document.write(
-      `${"<p>" + pokemonList[i].name} height is ${
-        pokemonList[i].height
-      }, color is ${pokemonList[i].color}, type is ${pokemonList[i].type}.`
+      `${"<p>" + pokemon.name} height is ${
+        pokemon.height
+      }: - Wow, that's big!, color is ${pokemon.color}, type is ${
+        pokemon.type
+      }.`
     );
   } else {
     document.write(
-      `${"<p>" + pokemonList[i].name} height is ${
-        pokemonList[i].height
-      }: - Wow, that's big!, color is ${pokemonList[i].color}, type is ${
-        pokemonList[i].type
-      }.`
+      `${"<p>" + pokemon.name} height is ${pokemon.height}, color is ${
+        pokemon.color
+      }, type is ${pokemon.type}.`
     );
   }
-}
+});
 
-// //FOR LOOP that iterates over each item in pokemonList (an array of objects) and document write () each Pokemon names.
+//FOR LOOP that iterates over each item in pokemonList (an array of objects) and document write () pokemon name, color, height, type and don't use console.log.
 // let pokemonList = [
 //   { name: "Bulbasaur", height: 7, color: "blue", type: "poison" },
 //   { name: "Charmander", height: 6, color: "orange", type: "fire" },
@@ -38,24 +32,26 @@ for (let i = 0; i < pokemonList.length; i++) {
 
 // for (let i = 0; i < pokemonList.length; i++) {
 //   if (pokemonList[i].height < 8 && pokemonList[i].height > 6) {
-//     document.write("<p>" + pokemonList[0].name + ", " + "<p/>");
-//     console.log(
+//     document.write(
 //       `${pokemonList[i].name} height is ${pokemonList[i].height}, color is ${pokemonList[i].color}, type is ${pokemonList[i].type}.`
 //     );
 //   } else if (pokemonList[i].height < 7 && pokemonList[i].height > 5) {
-//     document.write("<p>" + pokemonList[1].name + ", " + "<p/>");
-//     console.log(
-//       `${pokemonList[i].name} height is ${pokemonList[i].height}, color is ${pokemonList[i].color}, type is ${pokemonList[i].type}.`
+//     document.write(
+//       `${"<p>" + pokemonList[i].name} height is ${
+//         pokemonList[i].height
+//       }, color is ${pokemonList[i].color}, type is ${pokemonList[i].type}.`
 //     );
 //   } else {
-//     document.write("<p>" + pokemonList[2].name + ". " + "<p/>");
-//     console.log(
-//       `${pokemonList[i].name} height is ${pokemonList[i].height}: - Wow, that's big!, color is ${pokemonList[i].color}, type is ${pokemonList[i].type}.`
+//     document.write(
+//       `${"<p>" + pokemonList[i].name} height is ${
+//         pokemonList[i].height
+//       }: - Wow, that's big!, color is ${pokemonList[i].color}, type is ${
+//         pokemonList[i].type
+//       }.`
 //     );
 //   }
 // }
 
-//Better to use FOR OF LOOP:
 //FOR OF LOOP that iterates over each item in pokemonList (an array of objects)
 // let pokemonList = [
 //   { name: "Bulbasaur", height: 7, color: "blue", type: "poison" },
