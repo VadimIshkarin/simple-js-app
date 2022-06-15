b31f4ce6b876d894e994648b2ce5d8147da98686
VadimIshkarin <theishkarin@gmail.com>
2022-06-15 12:42:46 -0400

    In scripts.js file I wrapped my pokemonList array in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,31 @@
+//IIFE - pokemonList1
+let pokemonRepository = (function () {
+  let pokemonList1 = [
+    { name: "Bulbasaur", height: 7, color: "blue", type: "poison" },
+    { name: "Charmander", height: 6, color: "orange", type: "fire" },
+    { name: "Charizard", height: 8, color: "brown", type: ["flying", " fire"] },
+  ];
+  function add(pokemon) {
+    pokemonList1.push(pokemon);
+  }
+  function getAll() {
+    return pokemonList1;
+  }
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.add({
+  name: "Dino",
+  height: 11,
+  color: "green",
+  type: "fire",
+});
+
 //FOR EACH method that iterates over each item in pokemonList (an array of objects).
 let pokemonList = [
   { name: "Bulbasaur", height: 7, color: "blue", type: "poison" },
@@ -53,14 +81,20 @@ pokemonList.forEach(function (pokemon) {
 // }
 
 //FOR OF LOOP that iterates over each item in pokemonList (an array of objects)
-// let pokemonList = [
+// let animalList = [
 //   { name: "Bulbasaur", height: 7, color: "blue", type: "poison" },
 //   { name: "Charmander", height: 6, color: "orange", type: "fire" },
 //   { name: "Charizard", height: 8, color: "brown", type: "flying" },
 // ];
 
-// for (let pokemon of pokemonList) {
-//   console.log(
-//     `${pokemon.name} height is ${pokemon.height}, color is ${pokemon.color}, type is ${pokemon.type}.`
-//   );
+// for (let animal of animalList) {
+//   if (animal.height >= 8) {
+//     console.log(
+//       `${animal.name} height is ${animal.height}: - Wow, that's big!, color is ${animal.color}, type is ${animal.type}.`
+//     );
+//   } else {
+//     console.log(
+//       `${animal.name} height is ${animal.height}, color is ${animal.color}, type is ${animal.type}.`
+//     );
+//   }
 // }
